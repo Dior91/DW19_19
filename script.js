f2420627ab0b973a0a58ac4746c5bba69625efb5
Dior <badior.ba@gmail.com>
2022-06-27 14:26:38 +0200

    exo variables

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 console.log("test");
+/*
+commentaires
+nous allons apprendre
+1. LES VARIABLES
+1. LES CONDITIONS
+3. LES BOUCLES 
+4. LES FONCTIONS
 
+*/
+// les variables
+/* 
+1.pourquoi les variables? pour stocker les données  Ex: nom d'un jeu = "jeu du plus ou du moins";
+2. une variable: le nom de la variable et la valeur de la variable: tiroir qui porte un nom
+ex: tiroir A <= 10 valeur 
+Les chaines de caracteres contient des guillemées
+le numerique ne contient pas de guillemées
+asdditionner les nombres ca fonctionne
+additionner une chaine de caractere avec une autre fait la concetenation
+
+*/
+
+a=10 // 10 rentre dans la variable a
+b=50 
+c="80"
+b=90 // b va contenir 90
+console.error(a+parseInt(c))
+console.error(typeof(c))
+console.error(b)
+
+// LES VARIABLES
+
+SEL=100;
+SUCRE=80;
+console.error("nous avons du sel en q de" + SEL + ". Nous avons du seucre en q de" + SUCRE)
+
+SEL=80;
+SUCRE=100;
+console.error("nous avons du sel en q de" + SEL + ". Nous avons du seucre en q de" + SUCRE)
