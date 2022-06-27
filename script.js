f7c136b1f73f170915f98c240dae802644ecb4f5
Dior <badior.ba@gmail.com>
2022-06-27 16:42:33 +0200

    exo 3

--- script.js
+++ script.js
@@ -34,9 +34,50 @@ SEL=100;
 SUCRE=80;
 console.error("nous avons du sel en q de" + SEL + ". Nous avons du seucre en q de" + SUCRE);
 
-A = SEL;
+
+// VOTRE CODE INVERSE LES VARIABLES 
+TEMP = SEL; // TEMP SERT DE MEMOIRE
 SEL= SUCRE;
-SUCRE= A;
+SUCRE= TEMP;
 console.error("nous avons du sel en q de" + SEL + ". Nous avons du seucre en q de" + SUCRE);
+
+/* EXO 2
+1.Récuperer l'age de l'utilisateur via la commande prompt
+2.afficher l'age de l'utilisateur dans la console
+3.afficher quel age aura l'utilisateur dans 2 ans dans la console */
+
+age = prompt("veuillez entrer votre age?");
+
+console.error("l'utilisateur a " + age + "ans");
+
+console.error(typeof(age))
+
+age2 = parseInt(age) + 2;
+
+console.error ("l'utilisateur aura " + age2 + " dans 2 ans");
+
+/*exo 3
+1 demander un prix d'un article via la commande prompt
+2. afficher dans la console: l'article vaut 45 euros 
+3. afficher dans la console l'article vaut 50 euros ttc*/
+
+prix = prompt("veuillez entrer le prix de l'article?");
+console.error("l'article vaut " + prix + "euros");
+
+prixttc = (parseInt(prix))*1.2;
+
+console.error("l'article vaut " + prixttc + "euros TTC");
+
+
+
+
+
+
+
+
+
+
+
+
 
 
